14f22030066fc4403d28142c71073885a07d7d2a
melaniecristeche <u943142818@byui.edu>
2022-03-03 20:19:58 -0300

    colors and stuff

--- snake-incomplete/snake-incomplete/snake/constants.py
+++ snake-incomplete/snake-incomplete/snake/constants.py
@@ -11,6 +11,6 @@ FONT_SIZE = 15
 CAPTION = "Snake"
 SNAKE_LENGTH = 8
 WHITE = Color(255, 255, 255)
-RED = Color(255, 41, 134)
+RED = Color(199, 41, 255)
 YELLOW = Color(255,255,107)
 GREEN = Color(0, 255, 0)
